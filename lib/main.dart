74b465130e054749aa9de14aa528a4f9be6f61ca
HyeonjeongJang <hyeonjeong.jang.0302@gmail.com>
2025-03-18 11:53:00 +0900

    content bottom button

--- lib/main.dart
+++ lib/main.dart
@@ -178,51 +178,53 @@ class ContentBox extends StatelessWidget {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Image.asset(
-                            'assets/icons/like.png',
-                            width: 24.0,
-                            height: 24.0,
-                          ),
-                          SizedBox(width: 5.0),
-                          Text(
-                            '좋아요',
-                            style: TextStyle(
-                              fontSize: 16.0,
-                              color: Color(0xFF969696),
-                            ),
-                          ),
-                        ],
-                      ),
+                    ContentBottomButton(
+                      title: '좋아요',
+                      iconPath: 'assets/icons/like.png',
                     ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Image.asset(
-                            'assets/icons/comment.png',
-                            width: 24.0,
-                            height: 24.0,
-                          ),
-                          SizedBox(width: 5.0),
-                          Text(
-                            '댓글달기',
-                            style: TextStyle(
-                              fontSize: 16.0,
-                              color: Color(0xFF969696),
-                            ),
-                          ),
-                        ],
-                      ),
+                    ContentBottomButton(
+                      title: '댓글달기',
+                      iconPath: 'assets/icons/comment.png',
                     ),
                   ],
                 ),
               ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// 게시물 하단 버튼
+class ContentBottomButton extends StatelessWidget {
+  final String title;
+  final String iconPath;
+  const ContentBottomButton({
+    super.key,
+    required this.title,
+    required this.iconPath,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            iconPath,
+            width: 24.0,
+            height: 24.0,
+          ),
+          SizedBox(width: 5.0),
+          Text(
+            title,
+            style: TextStyle(
+              fontSize: 16.0,
+              color: Color(0xFF969696),
             ),
           ),
         ],
