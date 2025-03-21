d4130f2d8834d473341412f3a54bc3ed14f68ae6
HyeonjeongJang <hyeonjeong.jang.0302@gmail.com>
2025-03-21 11:21:56 +0900

    savepoint

--- lib/feature/presentation/pages/board_detail_page.dart
+++ lib/feature/presentation/pages/board_detail_page.dart
@@ -184,8 +184,8 @@ class BoardDetailPage extends HookConsumerWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Container(
-                  width: 40.0,
-                  height: 40.0,
+                  width: 32.0,
+                  height: 32.0,
                   decoration: BoxDecoration(
                     // color: Color(
                     //     int.parse('0xFF${content.iconColor}')),
@@ -202,74 +202,124 @@ class BoardDetailPage extends HookConsumerWidget {
                     style: TextStyle(color: Colors.white),
                   ),
                 ),
+                SizedBox(width: AppSizes.spacingM),
                 Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    // 댓글
+                    // 아이콘 오른쪽 영역
                     Column(
                       children: [
-                        Column(
-                          children: [
-                            Row(
+                        Container(
+                          width: 200.0,
+                          decoration: BoxDecoration(
+                            color: AppColor.bgGrey,
+                            borderRadius: BorderRadius.circular(
+                              AppSizes.borderRadiusL,
+                            ),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                              vertical: 8.0,
+                              horizontal: 12.0,
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                                Text(
-                                  '최희철',
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.w600,
-                                  ),
+                                // 이름, 날짜
+                                Row(
+                                  children: [
+                                    Text(
+                                      '최희철',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.w600,
+                                        fontSize: 14.0,
+                                      ),
+                                    ),
+                                    SizedBox(width: AppSizes.spacingS),
+                                    Text(
+                                      DateFormatter.formatToKoreanDateTime(
+                                          '2024-01-05T08:00:00'),
+                                      style: TextStyle(
+                                        color: AppColor.grey,
+                                        fontSize: 8.0,
+                                      ),
+                                    ),
+                                  ],
                                 ),
-                                Text(
-                                  DateFormatter.formatToKoreanDateTime(
-                                      '2024-01-05T08:00:00'),
-                                  style: TextStyle(
-                                    color: AppColor.grey,
-                                    fontSize: 12.0,
+                                // 댓글 내용
+                                Align(
+                                  alignment: Alignment.centerLeft,
+                                  child: RichText(
+                                    textAlign: TextAlign.left,
+                                    text: TextSpan(
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.w600,
+                                        fontSize: 14.0,
+                                      ),
+                                      children: [
+                                        TextSpan(
+                                          text: '',
+                                          style: TextStyle(
+                                              color: AppColor.mainBlue),
+                                        ),
+                                        TextSpan(
+                                            text: '네, 확인했습니다.\n저번주 지영님인거 같아요!',
+                                            style: TextStyle(
+                                                color: AppColor.black)),
+                                      ],
+                                    ),
                                   ),
                                 ),
                               ],
                             ),
-                            RichText(
-                              text: TextSpan(
-                                style: TextStyle(
-                                  fontWeight: FontWeight.w600,
-                                ),
-                                children: [
-                                  TextSpan(
-                                    text: '',
-                                    style: TextStyle(color: AppColor.mainBlue),
-                                  ),
-                                  TextSpan(
-                                      text: '네, 확인했습니다.\n저번주 지영님인거 같아요!',
-                                      style: TextStyle(color: AppColor.black)),
-                                ],
-                              ),
-                            ),
-                          ],
+                          ),
                         ),
                       ],
                     ),
+                    SizedBox(height: AppSizes.spacingS),
+                    // 좋아요, 답글달기
                     Row(
                       children: [
                         Row(
                           children: [
-                            Text('좋아요'),
-                            Icon(Icons.thumb_up),
-                            Text('2'),
+                            // 좋아요 했으면 글씨랑 아이콘 파란색
+                            Text(
+                              '좋아요',
+                              style: TextStyle(
+                                color: AppColor.mainBlue,
+                                fontSize: 12.0,
+                              ),
+                            ),
+                            Icon(Icons.thumb_up,
+                                color: AppColor.mainBlue, size: 15.0),
+                            SizedBox(width: 2.0),
+                            Text(
+                              '2',
+                              style: TextStyle(
+                                fontSize: 12.0,
+                              ),
+                            ),
                           ],
                         ),
+                        SizedBox(width: AppSizes.spacingM),
                         Row(
                           children: [
-                            Text('답글달기'),
+                            Text('답글달기',
+                                style: TextStyle(
+                                  fontSize: 12.0,
+                                )),
                           ],
                         ),
                       ],
                     ),
+                    SizedBox(height: AppSizes.spacingM),
                     // 대댓글
                     Row(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Container(
-                          width: 40.0,
-                          height: 40.0,
+                          width: 32.0,
+                          height: 32.0,
                           decoration: BoxDecoration(
                             // color: Color(
                             //     int.parse('0xFF${content.iconColor}')),
@@ -285,53 +335,93 @@ class BoardDetailPage extends HookConsumerWidget {
                             style: TextStyle(color: Colors.white),
                           ),
                         ),
+                        SizedBox(width: AppSizes.spacingM),
                         Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
                             // 댓글
-                            Column(
-                              children: [
-                                Column(
-                                  children: [
-                                    Row(
+                            Container(
+                              width: 200.0,
+                              decoration: BoxDecoration(
+                                color: AppColor.bgGrey,
+                                borderRadius: BorderRadius.circular(
+                                  AppSizes.borderRadiusL,
+                                ),
+                              ),
+                              child: Column(
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                      vertical: 8.0,
+                                      horizontal: 12.0,
+                                    ),
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
                                       children: [
-                                        Text('김철수'),
-                                        Text('2024-01-05T08:00:00'),
+                                        Row(
+                                          children: [
+                                            Text(
+                                              '김철수',
+                                              style: TextStyle(
+                                                  fontWeight: FontWeight.w600),
+                                            ),
+                                            SizedBox(width: AppSizes.spacingS),
+                                            Text(
+                                              DateFormatter
+                                                  .formatToKoreanDateTime(
+                                                      '2024-01-05T08:00:00'),
+                                              style: TextStyle(
+                                                color: AppColor.grey,
+                                                fontSize: 8.0,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                        Align(
+                                          alignment: Alignment.centerLeft,
+                                          child: RichText(
+                                            textAlign: TextAlign.left,
+                                            text: TextSpan(
+                                              style: TextStyle(
+                                                fontWeight: FontWeight.w600,
+                                              ),
+                                              children: [
+                                                TextSpan(
+                                                  text: '@최희철 ',
+                                                  style: TextStyle(
+                                                      color: AppColor.mainBlue),
+                                                ),
+                                                TextSpan(
+                                                    text: '오키 고마워요!',
+                                                    style: TextStyle(
+                                                        color: AppColor.black)),
+                                              ],
+                                            ),
+                                          ),
+                                        ),
                                       ],
                                     ),
-                                    RichText(
-                                      text: TextSpan(
-                                        style: TextStyle(
-                                          fontWeight: FontWeight.w600,
-                                        ),
-                                        children: [
-                                          TextSpan(
-                                            text: '@최희철 ',
-                                            style: TextStyle(
-                                                color: AppColor.mainBlue),
-                                          ),
-                                          TextSpan(
-                                              text: '오키 고마워요!',
-                                              style: TextStyle(
-                                                  color: AppColor.black)),
-                                        ],
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ],
+                                  ),
+                                ],
+                              ),
                             ),
+                            SizedBox(height: AppSizes.spacingS),
                             Row(
                               children: [
                                 Row(
                                   children: [
-                                    Text('좋아요'),
-                                    Icon(Icons.thumb_up),
-                                    Text('2'),
+                                    Text('좋아요',
+                                        style: TextStyle(fontSize: 12.0)),
+                                    Icon(Icons.thumb_up, size: 15.0),
+                                    Text('2', style: TextStyle(fontSize: 12.0)),
                                   ],
                                 ),
+                                SizedBox(width: AppSizes.spacingM),
                                 Row(
                                   children: [
-                                    Text('답글달기'),
+                                    Text('답글달기',
+                                        style: TextStyle(fontSize: 12.0)),
                                   ],
                                 ),
                               ],
